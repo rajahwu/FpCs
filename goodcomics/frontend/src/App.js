5ac5bd74ed7feb3187be1f44fb21d618c59f7e2c
rajahwu <vmradford@gmail.com>
2023-05-13 13:02:47 -0400

    create LandingPage component

--- goodcomics/frontend/src/App.js
+++ goodcomics/frontend/src/App.js
@@ -1,59 +1,8 @@
-import { useState, useEffect } from "react";
-import ContentCard from "./components/ContentCard";
-
-import {  getMarvelCharacters } from "./resources/marvel";
-
+import LandingPage from "./LandingPage";
 import "./App.css";
 
 function App() {
-  const [searchTerms, setSearchTerms] = useState({});
-  const [resource, setResource] = useState([]);
-  const [content, setContent] = useState([]);
-
-  useEffect(() => {
-    if (resource.data) {
-      console.log("useEffect resource", resource?.data.results);
-      setContent(resource.data.results);
-    }
-  }, [resource]);
-
-  function handleClick(e) {
-    e.preventDefault();
-    getMarvelCharacters(searchTerms).then((data) => setResource(data));
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>GoodComics</h1>
-        <form>
-        <p>Search Charcters</p>
-          <input
-            name="startsWith"
-            type="text"
-            placeholder="starts with"
-            value={searchTerms.startsWith ? searchTerms.startsWith : ""}
-            onChange={(e) =>
-              setSearchTerms({ searchTerms, ...{ startsWith: e.target.value } })
-            }
-          />
-          <button onClick={handleClick}>Search</button>
-        </form>
-
-        {content?.map((entry) => (
-          <div key={entry.id}>
-            <ContentCard
-              id={entry.id}
-              title={entry.name}
-              imageUrl={`${entry.thumbnail.path}.${entry.thumbnail.extension}`}
-              description={entry.description}
-              urls={entry.urls}
-            />
-          </div>
-        ))}
-      </header>
-    </div>
-  );
+  return <LandingPage />
 }
 
 export default App;
---
+++ goodcomics/frontend/src/LandingPage/index.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { ContentCard } from "../components";
+import { getMarvelCharacters } from "../resources/marvel";
+
+const SearchBar = ({ searchTerms, setResource, setSearchTerms }) => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    getMarvelCharacters(searchTerms).then((data) => setResource(data));
+  };
+
+  return (
+    <form>
+      <p>Search Charcters</p>
+      <input
+        name="startsWith"
+        type="text"
+        placeholder="starts with"
+        value={searchTerms.startsWith ? searchTerms.startsWith : ""}
+        onChange={(e) =>
+          setSearchTerms({ searchTerms, ...{ startsWith: e.target.value } })
+        }
+      />
+      <button onClick={handleClick}>Search</button>
+    </form>
+  );
+};
+
+export default function LandingPage() {
+  const [searchTerms, setSearchTerms] = useState({});
+  const [resource, setResource] = useState([]);
+  const [content, setContent] = useState([]);
+
+  useEffect(() => {
+    if (resource.data) {
+      console.log("useEffect resource", resource?.data.results);
+      setContent(resource.data.results);
+    }
+  }, [resource]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GoodComics</h1>
+          <SearchBar
+            searchTerms={searchTerms}
+            setSearchTerms={setSearchTerms}
+            setResource={setResource}
+          />
+       
+
+        {content?.map((entry) => (
+          <div key={entry.id}>
+            <ContentCard
+              id={entry.id}
+              title={entry.name}
+              imageUrl={`${entry.thumbnail.path}.${entry.thumbnail.extension}`}
+              description={entry.description}
+              urls={entry.urls}
+            />
+          </div>
+        ))}
+      </header>
+    </div>
+  );
+}
